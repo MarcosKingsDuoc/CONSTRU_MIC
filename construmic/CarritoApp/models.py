96d37eef476ff4efde47ff20d4d063889c069f3a
Marcos Reyes <astronautmarkus@gmail.com>
2024-05-07 22:51:04 -0400

    Logica de producto creada junto a carrito

--- construmic/CarritoApp/models.py
+++ construmic/CarritoApp/models.py
@@ -3,8 +3,21 @@ from django.db import models
 # Create your models here.
 class Producto(models.Model):
     nombre = models.CharField(max_length=64)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.CharField(max_length=32)
-    precio = models.IntegerField()
+    alto = models.IntegerField()
+    largo = models.IntegerField()
+    ancho = models.IntegerField()
+    peso = models.DecimalField(max_digits=6, decimal_places=2)
+    descripcion = models.TextField()
+    ESTADO_CHOICES = [
+        (1, 'Nuevo'),
+        (2, 'Usado'),
+        (3, 'Reacondicionado'),
+    ]
+    estado = models.IntegerField(choices=ESTADO_CHOICES)
+    imagen = models.BinaryField()
 
-def __str__(self):
-    return f'{self.nombre} -> {self.precio}'
+    def __str__(self):
+        return f'{self.nombre} -> {self.precio}'
+
--- construmic/construmic/views.py
+++ construmic/construmic/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+from CarritoApp.models import Producto
+
 def index_page(request):
     return render(request, 'index.html')
 
 def productos_page(request):
-    return render(request, 'productos.html')
+    producutos = Producto.objects.all()
+    return render(request, 'productos.html', {'productos':producutos})
 
 def ofertas_page(request):
     return render(request, 'ofertas.html')
