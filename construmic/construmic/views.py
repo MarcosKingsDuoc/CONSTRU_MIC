d3b9ecf933851d92fd040659a84536fb9ffa742a
Marcos Reyes <astronautmarkus@gmail.com>
2024-05-08 22:38:56 -0400

    Url contacto movida

--- construmic/construmic/views.py
+++ construmic/construmic/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 
 
 
-from ContactoApp.models import FormularioContacto 
+
 from CarritoApp.models import Producto
 
 def index_page(request):
@@ -19,25 +19,6 @@ def ofertas_page(request):
     return render(request, 'ofertas.html')
 
 
-def contacto_page(request):
-    if request.method == 'POST':
-        nombres = request.POST.get('nombres')
-        correo = request.POST.get('correo')
-        mensaje = request.POST.get('mensaje')
-        if nombres and correo and mensaje:
-            if len(mensaje) < 5:
-                # Hacer algo si el mensaje es demasiado corto
-                print("El mensaje es demasiado corto.")
-                # Puedes mostrar una alerta o realizar alguna otra acción
-                messages.warning(request, 'Su mensaje es muy corto. Por favor, escriba un mensaje de 5 o más carácteres.')
 
-            else:
-                contacto = FormularioContacto.objects.create(nombres=nombres, correo=correo, mensaje=mensaje)
-                contacto.save()
-                messages.success(request, '¡Mensaje enviado correctamente!')
-                return redirect('contacto_page')
-        else:
-            messages.error(request, 'Por favor, complete todos los campos.')
-    return render(request, 'contacto.html')
 
 
