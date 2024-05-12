3fad3f282f643d2811f12b9f7f5f58486cc858e4
Marcos Reyes <astronautmarkus@gmail.com>
2024-05-11 22:13:32 -0400

    issue de superusuario reparado

--- construmic/Accounts/models.py
+++ construmic/Accounts/models.py
@@ -30,6 +30,7 @@ class CustomUser(AbstractBaseUser):
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
